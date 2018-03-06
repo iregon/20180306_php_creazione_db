48445c0f535b9e2a44d59ab71cd7497d08b4db56
iregon <0iregon0@gmail.com>
2018-03-06 13:50:35 +0100

    Signed-off-by: iregon <0iregon0@gmail.com>

---
+++ creazione.php
@@ -0,0 +1,8 @@
+<?php
+    $nuovo = "db_bollette";
+    $t_utenti = "Utenti";
+    $t_credenziali = "Credenziali";
+    $t_bollette = "Bollette";
+
+    $conn = @mysqli_connect("localhost", "root", "");
+?>
